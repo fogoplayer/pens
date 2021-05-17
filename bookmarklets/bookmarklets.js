4a9d4764d69d4ebc6cf162c99d35e40b86ca4808
fogoplayer <zarinloosli@gmail.com>
2021-05-16 18:31:59 -0600

    Not sure what the difference is

--- bookmarklets/bookmarklets.js
+++ bookmarklets/bookmarklets.js
@@ -1,4 +1,4 @@
-function changeSpeed() {
+javascript: (function () {
   var speed = prompt("Enter new playback speed");
   document.querySelector("video").playbackRate = parseFloat(speed);
-}
+})();
