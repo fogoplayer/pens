b1ec47ec78a5da3c39709dda364ecd3fb7409d44
fogoplayer <zarinloosli@gmail.com>
2021-10-02 19:50:31 -0600

    Control player

---
+++ flappy-dom/Player.js
@@ -0,0 +1,54 @@
+const VH = window.innerHeight / 100;
+const G = 4.9 * 10 * VH;
+
+export default class Player {
+  constructor() {
+    this.player = document.getElementById("player");
+    this.y = window.innerHeight / 2;
+    document.addEventListener("keydown", (e) => {
+      this.flap();
+    });
+    setTimeout(() => {
+      this.fall();
+    }, 1000);
+  }
+
+  flap() {
+    console.log("flap");
+    const flapTime = 0.5;
+
+    //Transition
+    this.player.style.transitionDuration = `${flapTime}s`;
+    this.player.style.transitionTimingFunction = "ease-out";
+
+    // Position
+    this.y = this.y + 20 * VH + this.player.offsetHeight;
+    clearInterval(this.flapTime);
+    this.flapTime = setTimeout(() => {
+      this.fall();
+    }, flapTime * 1000);
+  }
+
+  fall() {
+    console.log("fall");
+
+    // Transition
+    const fallTime = Math.sqrt((2 * this.y) / G);
+    this.player.style.transitionDuration = `${fallTime}s`;
+    console.log("Fall Time:", fallTime);
+    this.player.style.transitionTimingFunction = "ease-in";
+
+    // Postition
+    this.y = 0;
+  }
+
+  get y() {
+    return window.innerHeight - this.player.offsetHeight - this.player.y;
+  }
+
+  set y(arg) {
+    this.player.style.top = `${
+      window.innerHeight - this.player.offsetHeight - arg
+    }px`;
+  }
+}
---
+++ flappy-dom/main.js
@@ -0,0 +1,3 @@
+import Player from "./Player.js";
+
+const player = new Player();
