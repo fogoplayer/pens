e23c9b0a1dde6bb3e97e7fcfa3eb063cdb402652
Zarin Loosli <zarinloosli@gmail.com>
2022-07-30 08:09:20 -0600

    Grow image

--- image-tiling/main.mjs
+++ image-tiling/main.mjs
@@ -1,13 +1,13 @@
 let aspectRatio = 1.5; // width / height
 const PAPER_SIZES = {
   // storing in pixels so that it's measurement-system-agnostic
-  letter: [ 816, 1056], // w / h
-  a4: [792, 1128]
+  letter: [816, 1056], // w / h
+  a4: [792, 1128],
 };
 const UNITS = {
   inches: 96, // px
   centimeters: 37.8, // px
-}
+};
 const form = document.querySelector("form");
 form.onsubmit = async (e) => {
   e.preventDefault();
@@ -17,37 +17,47 @@ form.onsubmit = async (e) => {
   }
 
   // Initialize variables
-  let {
-    image,
-    width,
-    height,
-    units,
-    paperSize,
-    orientation,
-    overlap,
-    margin
-  } = formData;
+  let { image, width, height, units, paperSize, orientation, overlap, margin } =
+    formData;
   let paperWidth, paperHeight;
-  
-  //Adjust variables
-  image = await getImageFromFile(image) // convert image to datstring
-  
-  // convert dimensions to px
-  width = width * UNITS[units]
-  height = height * UNITS[units]
-  overlap = overlap * UNITS[units]
-  margin = margin * UNITS[units]
-  if (orientation === "portrait") { // paper orientation
-    [paperWidth, paperHeight] = PAPER_SIZES[paperSize]
-  }else {
-    [paperHeight, paperWidth] = PAPER_SIZES[paperSize]
-  }
-  
-  const horizontalCount = getPageCount(width, paperWidth, overlap, margin)
-  const verticalCount = getPageCount(height, paperHeight, overlap, margin)
-  debugger
-};
 
+  //Adjust variables
+  // image = await getImageFromFile(image); // convert image to datstring
+  image = document.querySelector("#preview"); // convert image to datstring
+
+  // convert dimensions to px
+  width = width * UNITS[units];
+  height = height * UNITS[units];
+  overlap = overlap * UNITS[units];
+  margin = margin * UNITS[units];
+  if (orientation === "portrait") {
+    // paper orientation
+    [paperWidth, paperHeight] = PAPER_SIZES[paperSize];
+  } else {
+    [paperHeight, paperWidth] = PAPER_SIZES[paperSize];
+  }
+
+  image = resizeImg(image, width, height);
+
+  const horizontalCount = getPageCount(width, paperWidth, overlap, margin);
+  const verticalCount = getPageCount(height, paperHeight, overlap, margin);
+
+  for (let i = 0; i < verticalCount; i++) {
+    for (let j = 0; j < horizontalCount; j++) {
+      const canvas = cropImg(
+        image,
+        paperWidth * j, // add overlap/margin later
+        paperHeight * i,
+        width,
+        height
+      );
+      document.querySelector("#output").appendChild(canvas);
+      debugger;
+      // const img = document.createElement("image");
+      // img.src = img;
+    }
+  }
+};
 
 // Convert image to dataURL
 document.querySelector("#file").onchange = async (e) => {
@@ -60,9 +70,9 @@ document.querySelector("#file").onchange = async (e) => {
 
   img.src = await getImageFromFile(e.target.files[0]);
   img.style.display = "block";
-  await new Promise((resolve, reject)=>{
-    img.onload = resolve
-  })
+  await new Promise((resolve, reject) => {
+    img.onload = resolve;
+  });
   console.log(img.naturalWidth, img.naturalHeight);
   aspectRatio = img.naturalWidth / img.naturalHeight;
 };
@@ -86,10 +96,31 @@ function getPageCount(targetDim, paperDim, overlap, margin) {
   paperDim - overlap - margin gives us the amount of useable space
   targetDim / that gives us the number of page
   */
-  const unusablePerPage = 2 * (overlap + margin)
-  const usablePerPage = paperDim - unusablePerPage
-  const pageCount = targetDim / usablePerPage
-  return Math.ceil(pageCount)
+  const unusablePerPage = 2 * (overlap + margin);
+  const usablePerPage = paperDim - unusablePerPage;
+  const pageCount = targetDim / usablePerPage;
+  return Math.ceil(pageCount);
+}
+
+function resizeImg(img, width, height) {
+  // Shrink to canvas
+  var canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0, width, height);
+  return canvas;
+}
+
+function cropImg(img, x, y, width, height) {
+  // Shrink to canvas
+  var canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, x, y, width, height, 0, 0, width, height);
+  document.querySelector("#output").appendChild(canvas);
+  return canvas;
 }
 
 // Link dimensions
